070276feb34ea53b3c94fd00818530aedb9c3d38
Jamie Carter <jc2820@hotmail.com>
2021-04-13 17:48:42 +0100

    add option to switch bg colour of header

--- components/header.js
+++ components/header.js
@@ -1,32 +1,26 @@
 import styles from "../styles/header.module.css";
 import HeaderLink from "../components/headerLink";
 
-export default function Header() {
+export default function Header({ transparent }) {
   return (
-    <header className={styles.header}>
-      <h1 className={styles.heading}>Sienna</h1>
-      <ul className={styles.nav}>
-        <li>
-          <HeaderLink href="/">
-            <a>Home</a>
-          </HeaderLink>
-        </li>
-        <li>
-          <HeaderLink href="/projects">
-            <a>Projects</a>
-          </HeaderLink>
-        </li>
-        <li>
-          <HeaderLink href="/services">
-            <a>Services</a>
-          </HeaderLink>
-        </li>
-        <li>
-          <HeaderLink href="/contact">
-            <a>Contact</a>
-          </HeaderLink>
-        </li>
-      </ul>
+    <header
+      className={transparent ? styles.headerTransparent : styles.headerOpaque}
+    >
+      <p className={styles.heading}>Sienna</p>
+      <nav className={styles.nav}>
+        <HeaderLink href="/">
+          <a>Home</a>
+        </HeaderLink>
+        <HeaderLink href="/projects">
+          <a>Projects</a>
+        </HeaderLink>
+        <HeaderLink href="/services">
+          <a>Services</a>
+        </HeaderLink>
+        <HeaderLink href="/contact">
+          <a>Contact</a>
+        </HeaderLink>
+      </nav>
     </header>
   );
 }
