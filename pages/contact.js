9b1f8fd77bfedcb187d4d5d6f6b615cceb653ff0
Bethany Scott <bethanyioscott@hotmail.co.uk>
2021-04-14 16:31:44 +0100

    Create wave feature

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,10 @@
 import Layout from "../components/layout";
-// import styles from "../styles/contact.module.css";
+import styles from "../styles/contact.module.css";
 
 export default function Contact() {
   return (
       <Layout page={"Contact"}>
+        <img src="/images/waves.svg" className={styles.waves}/>
       </Layout>
   );
 }
