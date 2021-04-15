b5b8d6f469cc09d133679b89c47e12cdffd55ec3
Bethany Scott <bethanyioscott@hotmail.co.uk>
2021-04-15 14:50:28 +0100

    Add profile images

--- pages/contact.js
+++ pages/contact.js
@@ -4,6 +4,24 @@ import styles from "../styles/contact.module.css";
 export default function Contact() {
   return (
       <Layout page={"Contact"}>
+<div className={styles.contactContainer}> 
+        <div className={styles.info}> 
+            <div className={styles.who}>
+             <h2 className={styles.contactTitle}>Sienna are...</h2>
+             <p className={styles.contactDeets}>Bethany Scott and Jamie Carter</p>
+            </div>
+
+            <div className={styles.email}>
+              <h2 className={styles.contactTitle}>Contact Us  </h2>
+              <p className={styles.contactDeets}>info@sienna.dev</p>
+            </div>
+        </div>
+
+            <div className={styles.images}>
+              <img src="/images/bs-profile.png" />
+              <img src="/images/jc-profile.png" className={styles.jcImg}/>
+            </div>
+</div>
         <img src="/images/waves.svg" className={styles.waves}/>
       </Layout>
   );
