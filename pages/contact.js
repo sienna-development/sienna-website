968baf0561c3e530587e62f31a1726f313c47f34
Bethany Scott <bethanyioscott@hotmail.co.uk>
2021-04-20 15:16:14 +0100

    Rearrange contacts page and adjust wave animation to work best

--- pages/contact.js
+++ pages/contact.js
@@ -3,8 +3,8 @@ import styles from "../styles/contact.module.css";
 
 export default function Contact() {
   return (
-      <Layout page={"Contact"}>
-<div className={styles.contactContainer}> 
+      <Layout page={"Contact"} className={styles.page}>
+
         <div className={styles.info}> 
             <div className={styles.who}>
              <h2 className={styles.contactTitle}>Sienna are...</h2>
@@ -15,19 +15,14 @@ export default function Contact() {
               <h2 className={styles.contactTitle}>Contact Us  </h2>
               <p className={styles.contactDeets}>info@sienna.dev</p>
             </div>
-        </div>
 
             <div className={styles.images}>
               <img src="/images/bs-profile.png" />
               <img src="/images/jc-profile.png" className={styles.jcImg}/>
             </div>
-            </div>
-
-      
+        </div>
         <img src="/images/waves.svg" className={styles.waves}/>
-     
-
-
+    
       </Layout>
   );
 }
