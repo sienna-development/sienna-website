3036d06200e3fff8d117a43398fb8a7dd55cd86e
Bethany Scott <bethanyioscott@hotmail.co.uk>
2021-04-20 15:56:47 +0100

    Add colours to array for landing page

--- utils/canvasUtils.js
+++ utils/canvasUtils.js
@@ -1,6 +1,6 @@
-const colours = ["#11AD75", "#DE4747", "#A2ABFE", "#FCCDE6"];
+const colours = ["#11AD75", "#DE4747", "#A2ABFE", "#FCCDE6", "#FFE3BA", "#FFD600", "#FE961D", "#F55081", "#F55081", "#0926BD", "#24E8C4", "#AD119E", "#BAFFDA", "#D680FF", "#FFA98E"];
 
 export default function changeColour() {
-  return colours[Math.floor(Math.random() * 4)];
+  return colours[Math.floor(Math.random() * 15)];
 };
 
