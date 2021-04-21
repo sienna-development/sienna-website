a0aeb62823a4702069bc23d9f0a1d2a1c3f2aa8f
Jamie Carter <jc2820@hotmail.com>
2021-04-21 12:40:03 +0100

    tidy up component

--- pages/404.js
+++ pages/404.js
@@ -3,32 +3,30 @@ import styles from "../styles/Home.module.css";
 import CanvasDraw from "react-canvas-draw";
 import changeColour from "../utils/canvasUtils";
 import React from "react";
-import Image from "next/image"
+import Image from "next/image";
 
 export default function Custom404() {
   const [colour, setColour] = React.useState("#001AFF");
   return (
     <Layout page={"404"}>
       <CanvasDraw
-          className={styles.canvas}
-          onChange={() => setColour(changeColour())}
-          lazyRadius={0}
-          brushRadius={100}
-          hideGrid={true}
-          canvasWidth={"100vw"}
-          canvasHeight={"100vh"}
-          brushColor={colour}
-          hideInterface={false}
-        />
-        <div className={styles.taglineContainer}>
+        className={styles.canvas}
+        onChange={() => setColour(changeColour())}
+        lazyRadius={0}
+        brushRadius={100}
+        hideGrid={true}
+        canvasWidth={"100vw"}
+        canvasHeight={"100vh"}
+        brushColor={colour}
+        hideInterface={false}
+      />
+      <div className={styles.taglineContainer}>
         <Image
-            src={"/images/404-face.svg"}
-            // width={500}
-            // height={500}
-            layout="fill"
-            alt={"404: Page not found"}
-          />
-        </div>
+          src={"/images/404-face.svg"}
+          layout="fill"
+          alt={"404: Page not found"}
+        />
+      </div>
     </Layout>
   );
 }
